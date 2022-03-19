5e9c77b5ff5c9b76d997e31334f7a70933685c4b
Prasanna Sridharan <prasannas090801@gmail.com>
2022-03-19 10:26:04 +0530

    Before After Login

--- src/Pages/Farmer/Buy.js
+++ src/Pages/Farmer/Buy.js
@@ -12,7 +12,8 @@ import {
   MenuOptionGroup,
   MenuDivider,
   Button,
-  Input
+  Input,
+  Stack, Text
 } from "@chakra-ui/react";
 import { ChevronDownIcon, Search2Icon } from "@chakra-ui/icons";
 import SearchField from "react-search-field";
@@ -26,14 +27,17 @@ export default function Buy() {
     console.log(res.data.data)
   }, []);
   return (
-    <Flex flexDir="column" w="100%">  
+    
+    
+    <div>
+      {window.sessionStorage.getItem('token') ?<Flex flexDir="column" w="100%">  
       <Flex mt="5"  mx="30%">
         <Menu>
           <MenuButton as={Button} rightIcon={<ChevronDownIcon />} mr="5%" w="20%">
             Filter
           </MenuButton>
           <MenuList>
-            <MenuItem>Equipment Name</MenuItem>
+            <MenuItem>Equipment Name 223</MenuItem>
             <MenuItem>Distance Range</MenuItem>
             <MenuItem>Price</MenuItem>
             <MenuItem>Start Date</MenuItem>
@@ -50,6 +54,164 @@ export default function Buy() {
 
         ))}
       </Grid>
-    </Flex>
+    </Flex> :       <div>
+            <img alignContent='center'
+            src="https://www.jfarmservices.in/img/Eicher-Tractor-Implement-hire.jpg"
+            alt="First slide"
+            width='auto'
+            height='270'
+            />
+
+            <Flex width='120%' flexDir='column' justifyContent='center'>
+
+            <Stack
+            marginBottom={'10px'}>
+            <Text 
+             marginTop={'20px'} paddingLeft={'40px'} fontWeight={600} fontSize={'25'}>
+              <b>BENIFITS TO FARMERS </b>
+            </Text>
+            </Stack>
+            <hr></hr>
+
+            <Stack
+            marginLeft={50}
+            marginTop={5}
+            >
+
+            <Flex flexDir='row' alignContent='center' pad>
+                <Flex flexDir='column' width='40%' ml='3%'>
+                    <Flex flexDir='row' height='50%' mr='5%' mt='1%'>
+                        <span>
+                            <img mt='1%'
+                            src = 'https://www.jfarmservices.in/img/icon/avoid-capital-investment.png'
+                            ></img>
+                        </span>
+                        <span>
+                            <Text 
+                            marginTop={'10px'} paddingLeft={'20px'} fontWeight={400} fontSize={'18'}>
+                            Ideal alternative to avoid capital investment in farm machinery
+
+                            </Text>
+                        </span>
+                    
+                        <hr></hr>
+                    </Flex>
+                    <hr width='95%'></hr>
+                    <Flex flexDir='row' height='50%'  mr='5%' mt='1%'>
+                        <span>
+                            <img mt='1%'
+                            src = 'https://www.jfarmservices.in/img/icon/quality.png'
+                            ></img>
+                        </span>
+                        <span>
+                            <Text 
+                            marginTop={'10px'} paddingLeft={'20px'} fontWeight={400} fontSize={'18'}>
+                            Access to high quality services with mechanized equipment for all farming needs
+                            </Text>
+                        </span>
+                        <hr></hr>
+                    </Flex>
+                    <hr width='95%'></hr>
+                    <Flex flexDir='row' height='50%' mr='5%' mt='1%'>
+                        <span>
+                            <img mt='1%'
+                            src = 'https://www.jfarmservices.in/img/icon/increase-productivity.png'
+                            ></img>
+                        </span>
+                        <span>
+                            <Text 
+                            marginTop={'10px'} paddingLeft={'20px'} fontWeight={400} fontSize={'18'}>
+                            Reduce input costs and increase productivity
+                            </Text>
+                        </span>
+                        <hr></hr>
+                    </Flex>
+                    <hr width='95%'></hr>
+                    <Flex flexDir='row' height='100px' mr='5%' mt='1%' mb='0.5%'>
+                        <span>
+                            <img mt='1%'
+                            src = 'https://www.jfarmservices.in/img/icon/nearby.png'
+                            ></img>
+                        </span>
+                        <span>
+                            <Text 
+                            marginTop={'10px'} paddingLeft={'20px'} fontWeight={400} fontSize={'18'}>
+                            Identify and hire necessary implements for requirements ranging from land preparation to harvest / post–harvest
+                            </Text>
+                        </span>
+                        <hr></hr>
+                    </Flex>
+                    <hr width='95%'></hr>
+                </Flex>
+                <Flex flexDir='column' width='40%' ml='2%'>
+                    <Flex flexDir='row' height='50%' mr='15%' mt='1%'>
+                        <span>
+                            <img mt='1%'
+                            src = 'https://www.jfarmservices.in/img/icon/wide-range.png'
+                            ></img>
+                        </span>
+                        <span>
+                            <Text 
+                            marginTop={'10px'} paddingLeft={'20px'} fontWeight={400} fontSize={'18'}>
+                            Choose from a wide range of specialized equipment for each application
+                            </Text>
+                        </span>
+                        <hr></hr>
+                    </Flex>
+                    <hr width='85%'></hr>
+                    <Flex flexDir='row' height='50%' mr='15%' mt='1%'>
+                    <span>
+                        <img mt='1%'
+                        src = 'https://www.jfarmservices.in/img/icon/increase-productivity.png'
+                        ></img>
+                    </span>
+                    <span>
+                        <Text 
+                        marginTop={'10px'} paddingLeft={'20px'} fontWeight={400} fontSize={'18'}>
+                        Reduce input costs and increase productivity
+                        </Text>
+                    </span>
+                    <hr></hr>
+                    </Flex>
+                    <hr width='85%'></hr>
+                    <Flex flexDir='row' height='50%' mr='15%' mt='1%'>
+                        <span>
+                            <img mt='1%'
+                            src = 'https://www.jfarmservices.in/img/icon/operators.png'
+                            ></img>
+                        </span>
+                        <span>
+                            <Text 
+                            marginTop={'10px'} paddingLeft={'20px'} fontWeight={400} fontSize={'18'}>
+                            Get farming needs serviced by high quality operators
+                            </Text>
+                        </span>
+                        <hr></hr>
+                    </Flex>
+                    <hr width='85%'></hr>
+                    <Flex flexDir='row' height='100px' mr='15%' mt='1%' mb='0.5%'>
+                    <span>
+                        <img mt='1%'
+                        src = 'https://www.jfarmservices.in/img/icon/tafe-tamoto.png'
+                        ></img>
+                    </span>
+                    <span>
+                        <Text 
+                        marginTop={'10px'} paddingLeft={'20px'} fontWeight={400} fontSize={'18'}>
+                        Specialized implements best suited for your crops, soil type and weather confirmations in your area
+                        </Text>
+                    </span>
+                    <hr></hr>
+                    </Flex>
+                    <hr width='85%'></hr>
+
+                </Flex>
+            </Flex>  
+                
+            </Stack>
+            
+            </Flex>
+        </div>}
+    </div>
   );
 }
--- src/Pages/Farmer/Rent.js
+++ src/Pages/Farmer/Rent.js
@@ -43,6 +43,7 @@ import {
   Heading,
   ButtonGroup,
   Radio,
+  Stack, Text
 } from "@chakra-ui/react";
 import jwt_decode from "jwt-decode";
 import { postEquipment } from "../../redux/api/User/user";
@@ -116,133 +117,266 @@ export const Rent = () => {
     console.log("Remove image id", id);
   };
   return (
-    <Flex flexDir="column" width="100%">
-      <Formik
-        initialValues={initialValues}
-        onSubmit={onSubmit}
-        validationSchema={validationSchema}
+   <div>
+      {window.sessionStorage.getItem('token') ? 
+  <h1><Flex flexDir="column" width="100%">
+  <Formik
+    initialValues={initialValues}
+    onSubmit={onSubmit}
+    validationSchema={validationSchema}
+  >
+    {({ handleSubmit, values, errors }) => (
+      <>
+      <Box
+        borderWidth="1px"
+        rounded="lg"
+        shadow="1px 1px 3px rgba(0,0,0,0.3)"
+        w="60%"
+        p={6}
+        m="10px auto"
+        as="form"
+        onSubmit={handleSubmit}
+        boxShadow="3px 5px #dde9e0"
       >
-        {({ handleSubmit, values, errors }) => (
-          <>
-          <Box
-            borderWidth="1px"
-            rounded="lg"
-            shadow="1px 1px 3px rgba(0,0,0,0.3)"
-            w="60%"
-            p={6}
-            m="10px auto"
-            as="form"
-            onSubmit={handleSubmit}
-            boxShadow="3px 5px #dde9e0"
+        <SelectControl
+          style={{ marginTop: "10px" }}
+          label="Equipement Name"
+          name="equipment_name"
+          selectProps={{ placeholder: "Select Equipment Name" }}
+        >
+          <option value="Tractor">Trac0tor</option>
+          <option value="Axe">Axe</option>
+          <option value="Sprinkler">Sprinkler</option>
+          <option value="Spade">Spade</option>
+          <option value="Shears">Shears</option>
+          <option value="Fork">Fork</option>
+          <option value="WheelBarrow">WheelBarrow</option>
+
+        </SelectControl>
+        {/* ENter Specification Checkbox */}
+        {/* <InputControl name="Specification" label="Enter Specification" /> */}
+        <CheckboxContainer
+          name="specification"
+          label="Enter Specification"
+          style={{ marginTop: "10px" }}
+        >
+          <CheckboxControl name="toppings" value="chicken">
+            Size
+          </CheckboxControl>
+          <CheckboxControl name="toppings" value="ham">
+            Capacity
+          </CheckboxControl>
+          <CheckboxControl name="toppings" value="mushrooms">
+            Power
+          </CheckboxControl>
+          <CheckboxControl name="toppings" value="cheese">
+            Other
+          </CheckboxControl>
+        </CheckboxContainer>
+        <InputControl
+          name=" hiring_cost"
+          label="Enter Hiring Cost"
+          style={{ marginTop: "10px" }}
+        />
+        {/* <InputControl name="Quantity" label="Aadhar Number" /> */}
+
+        {/* Unit dropdown hourly/daily */}
+        <SelectControl
+          style={{ marginTop: "10px" }}
+          label="Enter Unit"
+          name="unit"
+          selectProps={{ placeholder: "Select Unit" }}
+        >
+          <option value="hourly">Hourly</option>
+          <option value="daily">Daily</option>
+        </SelectControl>
+
+          {/* Unit dropdown hourly/daily */}
+          <SelectControl
+            marginLeft={200}
+            style={{ marginTop: "10px" }}
+            width="60%"
+            label="Enter Unit"
+            name="unit"
+            selectProps={{ placeholder: "Select Unit" }}
           >
-            <SelectControl
-              style={{ marginTop: "10px" }}
-              label="Equipement Name"
-              name="equipment_name"
-              selectProps={{ placeholder: "Select Equipment Name" }}
-            >
-              <option value="Tractor">Trac0tor</option>
-              <option value="Axe">Axe</option>
-              <option value="Sprinkler">Sprinkler</option>
-              <option value="Spade">Spade</option>
-              <option value="Shears">Shears</option>
-              <option value="Fork">Fork</option>
-              <option value="WheelBarrow">WheelBarrow</option>
-
-            </SelectControl>
-            {/* ENter Specification Checkbox */}
-            {/* <InputControl name="Specification" label="Enter Specification" /> */}
-            <CheckboxContainer
-              name="specification"
-              label="Enter Specification"
-              style={{ marginTop: "10px" }}
-            >
-              <CheckboxControl name="toppings" value="chicken">
-                Size
-              </CheckboxControl>
-              <CheckboxControl name="toppings" value="ham">
-                Capacity
-              </CheckboxControl>
-              <CheckboxControl name="toppings" value="mushrooms">
-                Power
-              </CheckboxControl>
-              <CheckboxControl name="toppings" value="cheese">
-                Other
-              </CheckboxControl>
-            </CheckboxContainer>
-            <InputControl
-              name=" hiring_cost"
-              label="Enter Hiring Cost"
-              style={{ marginTop: "10px" }}
+            <option value="hourly">Hourly</option>
+            <option value="daily">Daily</option>
+          </SelectControl>
+        {/* </Flex> */}
+        <Flex flexDir="row">
+          <InputControl
+            width="60%"
+            style={{ marginTop: "10px" }}
+            name="start_date"
+            label="Start Date"
+            inputProps={{ type: "date" }}
+          />
+          <InputControl
+            marginLeft={200}
+            width="60%"
+            style={{ marginTop: "10px", marginBottom: "10 px" }}
+            name="end_date"
+            label="End Date"
+            inputProps={{ type: "date" }}
+          />
+        </Flex>
+        <RMIUploader
+          isOpen={visible}
+          hideModal={hideModal}
+          onSelect={onSelect}
+          onUpload={onUpload}
+          onRemove={onRemove}
+          dataSources={a}
+        />
+        <ButtonGroup ml="35%" mt="7">
+          <ResetButton mr="10" w="100px">
+            Reset
+          </ResetButton>
+          <SubmitButton
+            w="100px"
+            
+          
+          >
+            Submit
+          </SubmitButton>
+        </ButtonGroup>
+      </Box>
+      </>
+    )}
+  </Formik>
+</Flex></h1>:<div>
+            <img alignContent='center'
+            src="https://www.jfarmservices.in/img/MF-Tractor-Fleet.jpg"
+            alt="First slide"
+            width='auto'
+            height='270'
             />
-            {/* <InputControl name="Quantity" label="Aadhar Number" /> */}
 
-            {/* Unit dropdown hourly/daily */}
-            <SelectControl
-              style={{ marginTop: "10px" }}
-              label="Enter Unit"
-              name="unit"
-              selectProps={{ placeholder: "Select Unit" }}
+            <Flex width='120%' flexDir='column' justifyContent='center'>
+
+            <Stack
+            marginBottom={'10px'}>
+            <Text 
+             marginTop={'20px'} paddingLeft={'40px'} fontWeight={600} fontSize={'25'}>
+              <b>BENIFITS OF RENTERS </b>
+            </Text>
+            </Stack>
+            <hr></hr>
+
+            <Stack
+            marginLeft={50}
+            marginTop={5}
             >
-              <option value="hourly">Hourly</option>
-              <option value="daily">Daily</option>
-            </SelectControl>
 
-              {/* Unit dropdown hourly/daily */}
-              <SelectControl
-                marginLeft={200}
-                style={{ marginTop: "10px" }}
-                width="60%"
-                label="Enter Unit"
-                name="unit"
-                selectProps={{ placeholder: "Select Unit" }}
-              >
-                <option value="hourly">Hourly</option>
-                <option value="daily">Daily</option>
-              </SelectControl>
-            {/* </Flex> */}
-            <Flex flexDir="row">
-              <InputControl
-                width="60%"
-                style={{ marginTop: "10px" }}
-                name="start_date"
-                label="Start Date"
-                inputProps={{ type: "date" }}
-              />
-              <InputControl
-                marginLeft={200}
-                width="60%"
-                style={{ marginTop: "10px", marginBottom: "10 px" }}
-                name="end_date"
-                label="End Date"
-                inputProps={{ type: "date" }}
-              />
-            </Flex>
-            <RMIUploader
-              isOpen={visible}
-              hideModal={hideModal}
-              onSelect={onSelect}
-              onUpload={onUpload}
-              onRemove={onRemove}
-              dataSources={a}
-            />
-            <ButtonGroup ml="35%" mt="7">
-              <ResetButton mr="10" w="100px">
-                Reset
-              </ResetButton>
-              <SubmitButton
-                w="100px"
+            <Flex flexDir='row' alignContent='center' pad>
+                <Flex flexDir='column' width='40%' ml='3%'>
+                    <Flex flexDir='row' height='60px' mr='4%' mt='1%' mb='0%'>
+                        <span>
+                            <img mt='1%'
+                            src = 'https://www.jfarmservices.in/img/icons/feedback.png'
+                            ></img>
+                        </span>
+                        <span>
+                            <Text 
+                            marginTop={'10px'} paddingLeft={'20px'} fontWeight={400} fontSize={'18'}>
+                            Improve quality of services based on feedback from the JFarm Services platform
+                            </Text>
+                        </span>
+                        <hr></hr>
+                    </Flex>
+                    <hr width='95%'></hr>
+                    
+                    <Flex flexDir='row' height='60px'  mr='4%' mt='1%'>
+                        <span>
+                            <img mt='1%'
+                            src = 'https://www.jfarmservices.in/img/icons/cropping-cycle.png'
+                            ></img>
+                        </span>
+                        <span>
+                            <Text 
+                            marginTop={'10px'} paddingLeft={'20px'} fontWeight={400} fontSize={'18'}>
+                            Maximize earning potential by serving more farmers through out the cropping cycle
+                            </Text>
+                        </span>
+                        <hr></hr>
+                    </Flex>
+                    <hr width='95%'></hr>
+
+                    <Flex flexDir='row' height='60px' mr='4%' mt='1%'>
+                        <span>
+                            <img mt='1%'
+                            src = 'https://www.jfarmservices.in/img/icons/no-cost-specilised-equip.png'
+                            ></img>
+                        </span>
+                        <span>
+                            <Text 
+                            marginTop={'10px'} paddingLeft={'20px'} fontWeight={400} fontSize={'18'}>
+                            Communicate availability to new and specialized equipment farmers at no cost
+                            </Text>
+                        </span>
+                        <hr></hr>
+                    </Flex>
+                    <hr width='95%'></hr>
+                </Flex>
+                <Flex flexDir='column' width='40%' ml='2%'>
+                    
+                    <Flex flexDir='row' height='70px' mr='15%' mt='1%'>
+                    <span>
+                        <img mt='1%'
+                        src = 'https://www.jfarmservices.in/img/icons/agri-and-non-agri.png'
+                        ></img>
+                    </span>
+                    <span>
+                        <Text 
+                        marginTop={'10px'} paddingLeft={'20px'} fontWeight={400} fontSize={'18'}>
+                        Schedule rental operations for effective season and off-season planning for agricultural and non-agricultural requirement
+                        </Text>
+                    </span>
+                    <hr></hr>
+                    </Flex>
+                    <hr width='85%'></hr>
+                    <Flex flexDir='row' height='60px' mr='5%' mt='1%'>
+                        <span>
+                            <img mt='1%'
+                            src = 'https://www.jfarmservices.in/img/icons/profit.png'
+                            ></img>
+                        </span>
+                        <span>
+                            <Text 
+                            marginTop={'10px'} paddingLeft={'20px'} fontWeight={400} fontSize={'18'}>
+                            Better asset utilization leading to higher business profitability
+
+                            </Text>
+                        </span>
+                    
+                        <hr></hr>
+                    </Flex>
+                    <hr width='85%'></hr>
+                    <Flex flexDir='row' height='60px' mr='15%' mt='1%'>
+                    <span>
+                        <img mt='1%'
+                        src = 'https://www.jfarmservices.in/img/icons/expertise-and-support.png'
+                        ></img>
+                    </span>
+                    <span>
+                        <Text 
+                        marginTop={'10px'} paddingLeft={'20px'} fontWeight={400} fontSize={'18'}>
+                        Benefit from expertise and support of Agventure.
+                        </Text>
+                    </span>
+                    <hr></hr>
+                    </Flex>
+                    <hr width='85%'></hr>
+
+                </Flex>
+            </Flex>  
                 
-              
-              >
-                Submit
-              </SubmitButton>
-            </ButtonGroup>
-          </Box>
-          </>
-        )}
-      </Formik>
-    </Flex>
+            </Stack>
+            
+            </Flex>
+        </div>}
+   </div>
   );
 };
 
